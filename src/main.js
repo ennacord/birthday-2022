501bb8430ea9e200c83672374037e3e54afc5d87
dragonjet <jetriconew@gmail.com>
2022-01-01 22:59:00 -0800

    Vue plugin: mobile detection, no vue devtools

--- src/main.js
+++ src/main.js
@@ -3,8 +3,10 @@ import App from './App.vue';
 import './registerServiceWorker';
 import router from './router';
 import store from './store';
+import './plugins/mobile';
 import vuetify from './plugins/vuetify';
 
+Vue.config.devtools = false;
 Vue.config.productionTip = false;
 
 new Vue({
