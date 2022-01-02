d971a301c1fad6ce9b6aab03e15a51fed0124358
dragonjet <jetriconew@gmail.com>
2022-01-01 23:04:00 -0800

    Cleanup and Reorganize

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 import Vue from 'vue';
 import App from './App.vue';
 import './registerServiceWorker';
-import router from './router';
-import store from './store';
-import './plugins/mobile';
+import router from './plugins/router';
+import store from './plugins/store';
 import vuetify from './plugins/vuetify';
+import './plugins/mobile';
 
 Vue.config.devtools = false;
 Vue.config.productionTip = false;
