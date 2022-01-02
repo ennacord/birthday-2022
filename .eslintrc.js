ca4e1577eb81597f9b7f494bea3c90faece8bfe4
dragonjet <jetriconew@gmail.com>
2022-01-01 22:50:41 -0800

    Eslint, GitIgnore

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,8 @@ module.exports = {
     parser: 'babel-eslint',
   },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'object-curly-newline': 'off',
+    'no-console': 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
   },
 };
