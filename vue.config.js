57276b2846778421943ab45af8b24e6babc903a5
dragonjet <jetriconew@gmail.com>
2022-01-02 00:03:51 -0800

    Vue configs for game environment

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,22 @@
 const path = require('path');
 
+// eslint-disable-next-line import/no-extraneous-dependencies
+const webpack = require('webpack');
+
 module.exports = {
   outputDir: path.resolve(__dirname, 'docs'),
   lintOnSave: false,
   transpileDependencies: [
     'vuetify',
   ],
+  configureWebpack: {
+    plugins: [
+      new webpack.DefinePlugin({
+        CANVAS_RENDERER: JSON.stringify(true),
+        WEBGL_RENDERER: JSON.stringify(true),
+      }),
+    ],
+  },
   chainWebpack: (config) => {
     // HTML Title used for OpenGraph linters (social media links)
     config.plugin('html').tap((args) => {
@@ -24,6 +35,24 @@ module.exports = {
       };
       return args;
     });
+    // Disable hot reload
+    config.module
+      .rule('vue')
+      .use('vue-loader')
+      .tap((options) => {
+        // eslint-disable-next-line no-param-reassign
+        options.hotReload = false;
+        return options;
+      });
+    // Disable base64 image url-loader
+    config.module
+      .rule('images')
+      .use('url-loader')
+      .tap((options) => {
+        // eslint-disable-next-line no-param-reassign
+        options.limit = 1;
+        return options;
+      });
   },
   pwa: {
     name: 'Enna 2022',
