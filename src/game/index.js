8a2513c89009682356379eb304385e26e588a3d5
dragonjet <jetriconew@gmail.com>
2022-01-02 00:58:55 -0800

    Fullscreen only on mobile

--- src/game/index.js
+++ src/game/index.js
@@ -38,21 +38,24 @@ class EnnaBirthday {
             strokeThickness: 5,
           });
 
-          // When fullscreen and always lock to landscape
-          this.game.scale.once('enterfullscreen', () => {
-            // eslint-disable-next-line no-empty
-            try { this.game.scale.lockOrientation(Phaser.Scale.LANDSCAPE); } catch (error) {}
-            // eslint-disable-next-line no-empty
-            try { ScreenOrientation.lock('landscape'); } catch (error) {}
-            // eslint-disable-next-line no-empty
-            try { window.screen.orientation.lock('landscape'); } catch (error) {}
-          });
+          // Go fullscreen if not on desktop browser
+          if (!this.sys.game.device.os.desktop) {
+            // When fullscreen and always lock to landscape
+            this.game.scale.once('enterfullscreen', () => {
+              // eslint-disable-next-line no-empty
+              try { this.game.scale.lockOrientation(Phaser.Scale.LANDSCAPE); } catch (error) {}
+              // eslint-disable-next-line no-empty
+              try { ScreenOrientation.lock('landscape'); } catch (error) {}
+              // eslint-disable-next-line no-empty
+              try { window.screen.orientation.lock('landscape'); } catch (error) {}
+            });
 
-          // On click
-          this.input.on('pointerdown', () => {
-            // Attempt fullscreen
-            this.game.scale.startFullscreen();
-          });
+            // On click
+            this.input.on('pointerdown', () => {
+              // Attempt fullscreen
+              this.game.scale.startFullscreen();
+            });
+          }
         },
       },
     });
