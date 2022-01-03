8ac047ffb33898748870ca9ad436652b49b287fe
dragonjet <jetriconew@gmail.com>
2022-01-03 02:47:30 -0800

    Parallax update, Enna center

--- src/game/scenes/party.js
+++ src/game/scenes/party.js
@@ -9,23 +9,23 @@ class PartyScene extends Phaser.Scene {
     const centerY = height / 2;
 
     this.images = {
-      room: { level: 5, obj: this.add.image(centerX, centerY, 'room') },
-      artworks: { level: 5, obj: this.add.image(centerX, centerY, 'artworks') },
-      banner: { level: 4, obj: this.add.image(centerX, centerY, 'banner') },
-      cake: { level: 2, obj: this.add.image(centerX, centerY, 'cake') },
-      crowd1: { level: 1, obj: this.add.image(centerX, centerY, 'crowd1') },
-      crowd2: { level: 1, obj: this.add.image(centerX, centerY, 'crowd2') },
-      crowd3: { level: 2, obj: this.add.image(centerX, centerY, 'crowd3') },
-      crowd4: { level: 2, obj: this.add.image(centerX, centerY, 'crowd4') },
-      crowd5: { level: 3, obj: this.add.image(centerX, centerY, 'crowd5') },
-      enna: { level: 2, obj: this.add.image(centerX, centerY, 'enna') },
-      gifts: { level: 5, obj: this.add.image(centerX, centerY, 'gifts') },
-      nina: { level: 4, obj: this.add.image(centerX, centerY, 'nina') },
-      radio: { level: 3, obj: this.add.image(centerX, centerY, 'radio') },
-      reimu: { level: 4, obj: this.add.image(centerX, centerY, 'reimu') },
-      switch: { level: 3, obj: this.add.image(centerX, centerY, 'switch') },
-      table: { level: 2, obj: this.add.image(centerX, centerY, 'table') },
-      tv: { level: 3, obj: this.add.image(centerX, centerY, 'tv') },
+      room: { level: 3, obj: this.add.image(centerX, centerY, 'room') },
+      artworks: { level: 3, obj: this.add.image(centerX, centerY, 'artworks') },
+      gifts: { level: 3, obj: this.add.image(centerX, centerY, 'gifts') },
+      banner: { level: 2, obj: this.add.image(centerX, centerY, 'banner') },
+      nina: { level: 2, obj: this.add.image(centerX, centerY, 'nina') },
+      tv: { level: 1, obj: this.add.image(centerX, centerY, 'tv') },
+      reimu: { level: 1, obj: this.add.image(centerX, centerY, 'reimu') },
+      switch: { level: 1, obj: this.add.image(centerX, centerY, 'switch') },
+      crowd5: { level: 1, obj: this.add.image(centerX, centerY, 'crowd5') },
+      enna: { level: 0, obj: this.add.image(centerX, centerY, 'enna') },
+      radio: { level: 0, obj: this.add.image(centerX, centerY, 'radio') },
+      crowd4: { level: 0, obj: this.add.image(centerX, centerY, 'crowd4') },
+      crowd3: { level: -1, obj: this.add.image(centerX, centerY, 'crowd3') },
+      cake: { level: -1, obj: this.add.image(centerX, centerY, 'cake') },
+      table: { level: -1, obj: this.add.image(centerX, centerY, 'table') },
+      crowd1: { level: -2, obj: this.add.image(centerX, centerY, 'crowd1') },
+      crowd2: { level: -2, obj: this.add.image(centerX, centerY, 'crowd2') },
     };
 
     // Cake = Birthday Relay
@@ -54,8 +54,8 @@ class PartyScene extends Phaser.Scene {
 
     this.input.on('pointermove', (pointer) => {
       Object.values(this.images).forEach((image) => {
-        const newX = centerX + ((pointer.x - centerX) * (0.02 * (6 - image.level)));
-        const newY = centerY + ((pointer.y - centerY) * (0.01 * (6 - image.level))) - 20;
+        const newX = centerX + ((pointer.x - centerX) * (0.03 * image.level));
+        const newY = centerY + ((pointer.y - centerY) * (0.015 * image.level)) - 20;
         image.obj.setPosition(newX, newY);
       });
     });
