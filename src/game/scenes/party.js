2e33f5944084d7562b52b9dd1fd9bc8f94e33d1e
dragonjet <jetriconew@gmail.com>
2022-02-03 01:33:51 -0800

    Overhauled Party Scene

--- src/game/scenes/party.js
+++ src/game/scenes/party.js
@@ -1,136 +1,67 @@
 import Phaser from 'phaser';
-import Cursor2 from '../assets/cursor/cursor2.png';
+import Elements from '../data/elements';
+
+const INTENSITY_X = 0.006;
+const INTENSITY_Y = 0.003;
 
 class PartyScene extends Phaser.Scene {
-  containers = {};
+  movables = {};
 
   create() {
     const { width, height } = this.sys.game.canvas;
     const centerX = width / 2;
     const centerY = height / 2;
 
-    this.createContainers(6, 3);
-    this.createImage(-1, 0.5, 0.85, 'table');
-    this.createImage(-1, 0.5, 0.7, 'cake', 'Baking Relay', (image, label) => {
-      // Cake = Birthday Relay
-      image
-        .setInteractive({ cursor: `url(${Cursor2}), auto`, pixelPerfect: true })
-        .on('pointerover', () => {
-          image.setAngle((Math.random() * 17) - 8);
-          label.setVisible(true);
-        })
-        .on('pointerout', () => {
-          image.setAngle(0);
-          label.setVisible(false);
-        })
-        .on('pointerdown', () => {
-          this.game.vue.dialog = true;
-          this.game.vue.openProject = 'cake';
-        });
-    });
-    this.createImage(2, 0.1, 0.24, 'reimu', 'God Sees All', (image, label) => {
-      // Banner = Message Board
-      image
-        .setInteractive({ cursor: `url(${Cursor2}), auto`, pixelPerfect: true })
-        .on('pointerover', () => {
-          image.setAngle((Math.random() * 11) - 5);
-          label.setVisible(true);
-        })
-        .on('pointerout', () => {
-          image.setAngle(0);
-          label.setVisible(false);
-        })
-        .on('pointerdown', () => {
-          this.game.vue.dialog = true;
-          this.game.vue.openProject = 'banner';
-        });
-    });
-    this.createImage(3, 0.5, 0.5, 'room');
-    this.createImage(3, 0.5, 0.1, 'balloons');
-    this.createImage(2, 0.67, 0.49, 'gifts', 'Gallery', (image, label) => {
-      // Banner = Message Board
-      image
-        .setInteractive({ cursor: `url(${Cursor2}), auto`, pixelPerfect: true })
-        .on('pointerover', () => {
-          image.setAngle((Math.random() * 11) - 5);
-          label.setVisible(true);
-        })
-        .on('pointerout', () => {
-          image.setAngle(0);
-          label.setVisible(false);
-        })
-        .on('pointerdown', () => {
-          this.game.vue.dialog = true;
-          this.game.vue.openProject = 'banner';
-        });
-    });
-    this.createImage(-2, 0.73, 0.75, 'millie', 'God Sees All', (image, label) => {
-      // Banner = Message Board
-      image
-        .setInteractive({ cursor: `url(${Cursor2}), auto`, pixelPerfect: true })
-        .on('pointerover', () => {
-          image.setAngle((Math.random() * 11) - 5);
-          label.setVisible(true);
-        })
-        .on('pointerout', () => {
-          image.setAngle(0);
-          label.setVisible(false);
-        })
-        .on('pointerdown', () => {
-          this.game.vue.dialog = true;
-          this.game.vue.openProject = 'banner';
-        });
-    });
+    // Create game objects and placements
+    Object.entries(Elements)
+      .forEach(([key, { texture, x, y, z, scale, str, ox, oy, text, project }]) => {
+        const container = this.add.container(centerX, centerY).setDepth(z * 10);
+        // Image
+        const image = this.add.image((width * x) - centerX, (height * y) - centerY, texture || key)
+          .setOrigin(ox, oy);
+        if (scale) image.setScale(scale);
+        container.add(image);
+        // Interactive object
+        if (text && project) this.interactiveElement(container, image, text, project);
+        // Add to movable list
+        this.movables[key] = { container, str };
+      });
 
+    // Parallax
     this.input.on('pointermove', (pointer) => {
-      Object.entries(this.containers).forEach(([level, container]) => {
-        const newX = centerX - ((pointer.x - centerX) * (0.006 * level));
-        const newY = centerY - ((pointer.y - centerY) * (0.003 * level));
+      Object.values(this.movables).forEach(({ container, str }) => {
+        const newX = centerX - ((pointer.x - centerX) * (INTENSITY_X * str));
+        const newY = centerY - ((pointer.y - centerY) * (INTENSITY_Y * str));
         container.setPosition(newX, newY);
       });
     });
   }
 
-  createContainers(quantity, maxLevel) {
-    const { width, height } = this.sys.game.canvas;
-    const centerX = width / 2;
-    const centerY = height / 2;
-    let level = maxLevel;
-
-    for (let index = quantity - 1; index >= 0; index -= 1) {
-      const container = this.add.container(centerX, centerY);
-      this.containers[`${level}`] = container;
-      level -= 1;
-    }
+  interactiveElement(container, image, text, project) {
+    // Label
+    const label = this.createLabel(image.x, image.y, text)
+      .setDepth(2000 + image.depth);
+    container.add(label);
+    // Interaction
+    image
+      .setInteractive({ pixelPerfect: true })
+      .on('pointerover', () => {
+        image.setAngle((Math.random() * 3) - 1);
+        label
+          .setAngle((Math.random() * 11) - 5)
+          .setVisible(true);
+      })
+      .on('pointerout', () => {
+        image.setAngle(0);
+        label.setVisible(false);
+      })
+      .on('pointerdown', () => {
+        this.game.vue.dialog = true;
+        this.game.vue.openProject = project;
+      });
   }
 
-  // x and y are values from 0 to 1
-  createImage(level, x, y, texture, labelText, cb) {
-    const container = this.containers[`${level}`];
-    if (!container) {
-      console.error(`invalid level: ${level}`);
-      return;
-    }
-
-    const { width, height } = this.sys.game.canvas;
-
-    const imageX = (width * x) - (width / 2);
-    const imageY = (height * y) - (height / 2);
-
-    const image = this.add.image(imageX, imageY, texture);
-    if (texture === 'room') image.setScale(1.05);
-    container.add(image);
-
-    let label = null;
-    if (labelText) {
-      label = this.createLabel(imageX, imageY, labelText, -10);
-      container.add(label);
-    }
-
-    if (cb) cb(image, label);
-  }
-
-  createLabel(x, y, text, angle = 0) {
+  createLabel(x, y, text) {
     return this.add.text(x, y, text, {
       fontFamily: 'Londrina Solid',
       fontSize: 70,
@@ -139,9 +70,9 @@ class PartyScene extends Phaser.Scene {
       strokeThickness: 5,
     })
       .setOrigin(0.5, 0.5)
-      .setAngle(angle)
       .setVisible(false);
   }
+
 }
 
 export default PartyScene;
