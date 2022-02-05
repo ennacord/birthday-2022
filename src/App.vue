02f4d016e810921e6af57c4a5a2606d19801398d
dragonjet <jetriconew@gmail.com>
2022-02-05 02:00:06 -0800

    Project modal sizing

--- src/App.vue
+++ src/App.vue
@@ -1,58 +1,85 @@
 <template>
   <v-app id="app">
     <div id="game-container" ref="game-container"></div>
-    <v-dialog v-model="dialog" id="projects" elevation="0">
+    <v-dialog id="projects" v-model="dialog"
+      elevation="0" overlay-color="purple darken-4" overlay-opacity="0.85">
       <v-card tile class="pa-4">
-        <div v-if="openProject === 'bakingrelay'" class="text-center">
+        <div v-if="openProject === 'bakingrelay'" class="project text-center">
           <iframe
-            width="854" height="480"
+            width="100%" height="100%"
             src="https://www.youtube.com/embed/gj4cVOgEGGM"
             frameborder="0"
             allow="autoplay; clipboard-write; encrypted-media; picture-in-picture"
             allowfullscreen>
           </iframe>
         </div>
-        <div v-if="openProject === 'gsacover'" class="text-center">
+        <div v-if="openProject === 'gsacover'" class="project">
+          <div class="project-description">
+            <h2>Baking Relay</h2>
+            <p class="mt-4">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Pellentesque eu semper nulla. Suspendisse lectus odio,
+              venenatis vitae enim non, eleifend mollis libero.
+              Nulla nec tempor erat.
+            </p>
+            <p>
+              Project Lead: <strong>BONER</strong>
+            </p>
+            <p>
+              Contributors: <strong>Lorem, ipsum dolor, sit amet, consectetur, adipiscing, elit,
+              Pellentesque, eu semper, nulla, Suspendisse, lectus odio,
+              venenatis, vitae, enim non, eleifend, mollis, libero,
+              Nulla, nec tempor, erat</strong>
+            </p>
+          </div>
+          <div class="project-close">
+            <v-btn
+              @click="dialog = false"
+              block large elevation="1" color="purple lighten-4">
+              Close
+            </v-btn>
+          </div>
+          <div class="project-content text-right">
+            <iframe
+              width="100%" height="100%"
+              src="https://www.youtube.com/embed/gj4cVOgEGGM"
+              frameborder="0"
+              allow="autoplay; clipboard-write; encrypted-media; picture-in-picture"
+              allowfullscreen>
+            </iframe>
+          </div>
+        </div>
+        <div v-if="openProject === 'aloucast'" class="project">
           <iframe
-            width="854" height="480"
+            width="100%" height="100%"
             src="https://www.youtube.com/embed/gj4cVOgEGGM"
             frameborder="0"
             allow="autoplay; clipboard-write; encrypted-media; picture-in-picture"
             allowfullscreen>
           </iframe>
         </div>
-        <div v-if="openProject === 'aloucast'" class="text-center">
+        <div v-if="openProject === 'rickroll'" class="project text-center">
           <iframe
-            width="854" height="480"
+            width="100%" height="100%"
             src="https://www.youtube.com/embed/gj4cVOgEGGM"
             frameborder="0"
             allow="autoplay; clipboard-write; encrypted-media; picture-in-picture"
             allowfullscreen>
           </iframe>
         </div>
-        <div v-if="openProject === 'rickroll'" class="text-center">
-          <iframe
-            width="854" height="480"
-            src="https://www.youtube.com/embed/gj4cVOgEGGM"
-            frameborder="0"
-            allow="autoplay; clipboard-write; encrypted-media; picture-in-picture"
-            allowfullscreen>
-          </iframe>
-        </div>
-        <div v-if="openProject === 'mural'" class="text-center">
+        <div v-if="openProject === 'mural'" class="project text-center">
           <v-img
             contain
             src="https://100k.ennaalouette.com/img/mural.4f63dd0c.jpg"
-            height="80vh"
           ></v-img>
         </div>
-        <div v-if="openProject === 'messages'" class="text-center">
+        <div v-if="openProject === 'messages'" class="project text-center">
           messages
         </div>
-        <div v-if="openProject === 'gallery'" class="text-center">
+        <div v-if="openProject === 'gallery'" class="project text-center">
           twitter hashtag gallery
         </div>
-        <div v-if="openProject === 'quests'" class="text-center">
+        <div v-if="openProject === 'quests'" class="project text-center">
           quests
         </div>
       </v-card>
@@ -102,15 +129,32 @@ body {
   #app {
     background:none;
   }
-  #projects {
-    .aspectRatio {
-      width:100%;
-      height:100%;
-      div {
-        width: 100%;
-        padding-bottom: 43.75%;
-        background:#fcc;
-      }
+  .v-dialog {
+    overflow:hidden;
+  }
+  .project {
+    height:80vh;
+    position:relative;
+    .project-description {
+      position:absolute;
+      top: 0;
+      left: 0;
+      width: 360px;
+      bottom: 50px;
+      overflow-y:scroll;
+    }
+    .project-content {
+      position:absolute;
+      top: 0;
+      left: 400px;
+      right: 0;
+      bottom: 0;
+    }
+    .project-close {
+      position:absolute;
+      left:0;
+      bottom:0;
+      width:360px;
     }
   }
 }
