f96ecc948dc8e9219b436d555974ca129baf223e
dragonjet <jetriconew@gmail.com>
2022-02-16 19:01:48 -0800

    Remove unsued modules

--- src/main.js
+++ src/main.js
@@ -2,8 +2,6 @@ import Vue from 'vue';
 import { VueMasonryPlugin } from 'vue-masonry';
 import App from './App.vue';
 import './registerServiceWorker';
-import router from './plugins/router';
-import store from './plugins/store';
 import vuetify from './plugins/vuetify';
 import './plugins/mobile';
 
@@ -13,8 +11,6 @@ Vue.config.devtools = false;
 Vue.config.productionTip = false;
 
 new Vue({
-  router,
-  store,
   vuetify,
   render: (h) => h(App),
 }).$mount('#app');
